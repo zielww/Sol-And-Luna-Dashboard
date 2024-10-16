aa4afc6ef2d34b75c0f72451d60479fe9fc0aa46
Patriach Cain <youanybluesky10@gmail.com>
2024-10-16 13:57:59 +0800

    added sorting in orders

--- Http/controllers/orders/index.php
+++ Http/controllers/orders/index.php
@@ -8,21 +8,38 @@ $admin = Session::get('admin');
 
 $db = App::resolve(Database::class);
 
-//Get Products
-$orders = $db->query("
+$status = match ($_GET['sort'] ?? false) {
+    "new" => 'new',
+    "processing" => 'processing',
+    "shipped" => 'shipped',
+    "delivered" => 'delivered',
+    "cancelled" => 'cancelled',
+    default => null,
+};
+
+$query = "
     SELECT order_items.*, products.*, orders.created_at, orders.status, orders.user
     FROM order_items
     JOIN products ON order_items.product_id = products.product_id
     JOIN orders ON order_items.order_id = orders.order_id
-")->get();
+";
 
-$open_orders = array_filter($orders, fn($order) => strtolower($order['status']) !== 'delivered');
+if ($status) {
+    $query .= " WHERE orders.status = '$status'";
+}
 
-$total_price =  array_reduce($orders, function($carry, $item) {
+$orders = $db->query($query)->get();
+
+$open_orders = array_filter($orders, function ($order) {
+    return strtolower($order['status']) !== 'delivered' && strtolower($order['status']) !== 'cancelled';
+});
+
+$total_price = array_reduce($orders, function ($carry, $item) {
     return $carry + ($item['quantity'] * floatval($item['price']));
 }, 0);
 
 $success_message = \Core\Session::get('success') ?? '';
 $error_message = \Core\Session::get('errors') ?? [];
+
 
 require base_path('Http/views/orders/index.php');
--- Http/views/partials/aside.php
+++ Http/views/partials/aside.php
@@ -1,7 +1,7 @@
 <?php
 //Get Orders
 use Core\App;
-$orders = App::resolve(\Core\Database::class)->query("
+$items = App::resolve(\Core\Database::class)->query("
     SELECT order_items.*, products.*, orders.created_at, orders.status, orders.user
     FROM order_items
     JOIN products ON order_items.product_id = products.product_id
@@ -47,7 +47,7 @@ $orders = App::resolve(\Core\Database::class)->query("
                     </svg>
                     <span class="flex-1 ms-3 whitespace-nowrap text-sm">Orders</span>
                     <span class="inline-flex items-center justify-center w-3 h-3 p-3 ms-3 text-sm font-medium
-                    text-primary-orange bg-primary-orange-2 rounded-full dark:bg-blue-900 dark:text-blue-300"><?= isset($orders) ? count($orders) : 3 ?></span>
+                    text-primary-orange bg-primary-orange-2 rounded-full dark:bg-blue-900 dark:text-blue-300"><?= isset($items) ? count($items) : 3 ?></span>
                 </a>
             </li>
             <li>
