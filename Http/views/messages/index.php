6f1b3ee36daf419ea4c1b4b7bb713c2abee60423
Patriach Cain <youanybluesky10@gmail.com>
2024-11-01 21:20:57 +0800

    finished first phase of messaging feature

--- Http/views/messages/index.php
+++ Http/views/messages/index.php
@@ -159,7 +159,7 @@ require base_path("Http/views/partials/main.php");
                 <?php endforeach; ?>
             </div>
             <!--            Chat Form-->
-            <form class="w-full h-full">
+            <form method="POST" action="<?= $_SERVER['REQUEST_URI'] ?>" class="w-full h-full">
                 <label for="chat" class="sr-only">Your message</label>
                 <div class="w-full h-full flex items-center px-3 py-2 rounded-lg dark:bg-gray-700">
                     <button type="button"
@@ -184,7 +184,7 @@ require base_path("Http/views/partials/main.php");
                         </svg>
                         <span class="sr-only">Add emoji</span>
                     </button>
-                    <textarea id="chat" rows="1"
+                    <textarea id="chat" rows="1" name="chat"
                               class="block mx-4 p-2.5 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                               placeholder="Your message..."></textarea>
                     <button type="submit"
